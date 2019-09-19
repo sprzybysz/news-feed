1d357994d30033e84dee28ae54ae793218d74e1f
Przybysz <sabina.przybysz@capgemini.com>
2019-09-19 16:42:32 +0200

    Add views and public folders

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const http = require('http');
+fs = require('fs');
+path = require('path');
+
 require('dotenv').config();
 
-const server = http.createServer((req, res) => {
+const servePage = (res, pageName) => {
     res.writeHead(200);
+    let stream = fs.createReadStream('views/' + pageName);
+    stream.pipe(res);
+}
+
+const serverPublicFile = (res, url) => {
+    res.writeHead(200);
+    let stream = fs.createReadStream(path.join(__dirname, url));
+    stream.pipe(res);
+}
+
+const server = http.createServer((req, res) => {
+
+    if (req.url.startsWith('/public')) {
+        serverPublicFile(res, req.url);
+        return;
+    }
 
     switch(req.url) {
         case '/admin':
-            res.end('Admin page');
+            servePage(res, 'admin.html');
+            break;
+        case '/settings':
+            servePage(res, 'settings.html')
             break;
         default:
-            res.end('News page');
+            servePage(res, 'home.html')
             break;
 
     }
